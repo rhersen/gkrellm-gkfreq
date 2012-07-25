88314a24c40d1d6e7e5ec6c4dda6d0e8c8fccedc
Rudolf Hersén <rhersen@gmail.com>
2012-07-25 07:45:43 +0200

    refactoring and cleanup

--- Makefile
+++ Makefile
@@ -1,49 +1,49 @@
 all: gkfreq_cpu0.so gkfreq_cpu1.so gkfreq_cpu2.so gkfreq_cpu3.so gkfreq_cpu4.so gkfreq_cpu5.so gkfreq_cpu6.so gkfreq_cpu7.so
 
 gkfreq_cpu0.o: gkfreq_cpu.c
-	gcc -fPIC -O2 -Wall `pkg-config gtk+-2.0 --cflags` -DCPU=\"cpu0\" -o gkfreq_cpu0.o -c gkfreq_cpu.c
+	gcc -fPIC -O2 -Wall -std=gnu1x `pkg-config gtk+-2.0 --cflags` -DCPU=\"cpu0\" -o gkfreq_cpu0.o -c gkfreq_cpu.c
 
 gkfreq_cpu0.so: gkfreq_cpu0.o
 	gcc -shared -o gkfreq_cpu0.so gkfreq_cpu0.o
 
 gkfreq_cpu1.o: gkfreq_cpu.c
-	gcc -fPIC -O2 -Wall `pkg-config gtk+-2.0 --cflags` -DCPU=\"cpu1\" -o gkfreq_cpu1.o -c gkfreq_cpu.c
+	gcc -fPIC -O2 -Wall -std=gnu1x `pkg-config gtk+-2.0 --cflags` -DCPU=\"cpu1\" -o gkfreq_cpu1.o -c gkfreq_cpu.c
 
 gkfreq_cpu1.so: gkfreq_cpu1.o
 	gcc -shared -o gkfreq_cpu1.so gkfreq_cpu1.o
 
 gkfreq_cpu2.o: gkfreq_cpu.c
-	gcc -fPIC -O2 -Wall `pkg-config gtk+-2.0 --cflags` -DCPU=\"cpu2\" -o gkfreq_cpu2.o -c gkfreq_cpu.c
+	gcc -fPIC -O2 -Wall -std=gnu1x `pkg-config gtk+-2.0 --cflags` -DCPU=\"cpu2\" -o gkfreq_cpu2.o -c gkfreq_cpu.c
 
 gkfreq_cpu2.so: gkfreq_cpu2.o
 	gcc -shared -o gkfreq_cpu2.so gkfreq_cpu2.o
 
 gkfreq_cpu3.o: gkfreq_cpu.c
-	gcc -fPIC -O2 -Wall `pkg-config gtk+-2.0 --cflags` -DCPU=\"cpu3\" -o gkfreq_cpu3.o -c gkfreq_cpu.c
+	gcc -fPIC -O2 -Wall -std=gnu1x `pkg-config gtk+-2.0 --cflags` -DCPU=\"cpu3\" -o gkfreq_cpu3.o -c gkfreq_cpu.c
 
 gkfreq_cpu3.so: gkfreq_cpu3.o
 	gcc -shared -o gkfreq_cpu3.so gkfreq_cpu3.o
 
 gkfreq_cpu4.o: gkfreq_cpu.c
-	gcc -fPIC -O2 -Wall `pkg-config gtk+-2.0 --cflags` -DCPU=\"cpu4\" -o gkfreq_cpu4.o -c gkfreq_cpu.c
+	gcc -fPIC -O2 -Wall -std=gnu1x `pkg-config gtk+-2.0 --cflags` -DCPU=\"cpu4\" -o gkfreq_cpu4.o -c gkfreq_cpu.c
 
 gkfreq_cpu4.so: gkfreq_cpu4.o
 	gcc -shared -o gkfreq_cpu4.so gkfreq_cpu4.o
 
 gkfreq_cpu5.o: gkfreq_cpu.c
-	gcc -fPIC -O2 -Wall `pkg-config gtk+-2.0 --cflags` -DCPU=\"cpu5\" -o gkfreq_cpu5.o -c gkfreq_cpu.c
+	gcc -fPIC -O2 -Wall -std=gnu1x `pkg-config gtk+-2.0 --cflags` -DCPU=\"cpu5\" -o gkfreq_cpu5.o -c gkfreq_cpu.c
 
 gkfreq_cpu5.so: gkfreq_cpu5.o
 	gcc -shared -o gkfreq_cpu5.so gkfreq_cpu5.o
 
 gkfreq_cpu6.o: gkfreq_cpu.c
-	gcc -fPIC -O2 -Wall `pkg-config gtk+-2.0 --cflags` -DCPU=\"cpu6\" -o gkfreq_cpu6.o -c gkfreq_cpu.c
+	gcc -fPIC -O2 -Wall -std=gnu1x `pkg-config gtk+-2.0 --cflags` -DCPU=\"cpu6\" -o gkfreq_cpu6.o -c gkfreq_cpu.c
 
 gkfreq_cpu6.so: gkfreq_cpu6.o
 	gcc -shared -o gkfreq_cpu6.so gkfreq_cpu6.o
 
 gkfreq_cpu7.o: gkfreq_cpu.c
-	gcc -fPIC -O2 -Wall `pkg-config gtk+-2.0 --cflags` -DCPU=\"cpu7\" -o gkfreq_cpu7.o -c gkfreq_cpu.c
+	gcc -fPIC -O2 -Wall -std=gnu1x `pkg-config gtk+-2.0 --cflags` -DCPU=\"cpu7\" -o gkfreq_cpu7.o -c gkfreq_cpu.c
 
 gkfreq_cpu7.so: gkfreq_cpu7.o
 	gcc -shared -o gkfreq_cpu7.so gkfreq_cpu7.o
--- gkfreq_cpu.c
+++ gkfreq_cpu.c
@@ -31,85 +31,73 @@
  */
 
 #include <gkrellm2/gkrellm.h>
-#include <stdio.h>
-#include <sys/time.h>
-#include <string.h>
-#include <unistd.h>
 
-#define	CONFIG_NAME	"gkfreq_" CPU
-#define	STYLE_NAME	"gkfreq_" CPU
+#define CONFIG_NAME "gkfreq_" CPU
+#define STYLE_NAME "gkfreq_" CPU
+#define DIR "/sys/devices/system/cpu/"
+#define CUR_FREQ "/cpufreq/scaling_cur_freq"
 
 static GkrellmMonitor	*monitor;
 static GkrellmPanel	*panel;
-static GkrellmDecal	*decal_text1;
+static GkrellmDecal	*text;
 static gint	style_id;
 
-
-__inline__ unsigned long long int rdtsc()
-{
-	unsigned long long int x;
-	__asm__ volatile (".byte 0x0f, 0x31" : "=A" (x));
-	return x;
-}
-
-static
-void read_MHz(char* buffer_, size_t bufsz_)
-{
-   FILE *f;
-   if ( (f = fopen("/sys/devices/system/cpu/" CPU "/cpufreq/scaling_cur_freq", "r")) == NULL) {
-
-       snprintf(buffer_, bufsz_, "n/a MHz");
-   }
-   else {
-       int i;
-       fscanf(f, "%d", &i );
-       snprintf(buffer_, bufsz_, "%d MHz", i/1000 );
-       fclose(f);
-   }
-}
-
-static gint
-panel_expose_event(GtkWidget *widget, GdkEventExpose *ev) {
-
-	gdk_draw_pixmap(widget->window,
-			widget->style->fg_gc[GTK_WIDGET_STATE (widget)],
-			panel->pixmap, ev->area.x, ev->area.y, ev->area.x, ev->area.y,
-			ev->area.width, ev->area.height);
-	return FALSE;
-}
-
+static gint w;
+static gchar  info[32];
 
 static void
 update_plugin() {
+  void read_mhz() {
+    FILE *f;
+    if (!(f = fopen(DIR CPU CUR_FREQ, "r"))) {
+      snprintf(info, 32, "no " CPU);
+    }
+    else {
+      int i;
+      int n = fscanf(f, "%d", &i );
+      if (n != 1) {
+        snprintf(info, 32, "n/a MHz");
+      } else {
+        snprintf(info, 32, "%4.1f GHz", i * 1e-6);
+      }
 
-	static gint	x_scroll, w;
-	static gchar  info[32];
+      fclose(f);
+    }
+  }
 
-	if (w == 0)
-		w = gkrellm_chart_width();
-	x_scroll = (x_scroll + 1) % (2 * w);
+  if (w == 0)
+    w = gkrellm_chart_width();
 	
-	// dont do it too much...
-	if ((GK.timer_ticks % 10) != 0) return;
+  // dont do it too much...
+  if (GK.timer_ticks % 10) return;
 
-	read_MHz(info, 31);
+  read_mhz();
 
-	decal_text1->x_off =
-		(w - gdk_string_width(decal_text1->text_style.font,
-		                      info)) / 2;
-	if (decal_text1->x_off < 0)
-		decal_text1->x_off = 0;
+  text->x_off =
+    (w - gdk_string_width((GdkFont*) text->text_style.font,
+                          info)) / 2;
+  if (text->x_off < 0)
+    text->x_off = 0;
 
-	gkrellm_draw_decal_text(panel, decal_text1, info, w - x_scroll);
-	gkrellm_draw_panel_layers(panel);
+  gkrellm_draw_decal_text(panel, text, info, w);
+  gkrellm_draw_panel_layers(panel);
 }
 
 
 static void
 create_plugin(GtkWidget *vbox, gint first_create) {
+  gint panel_expose_event(GtkWidget *widget, GdkEventExpose *ev) {
+    gdk_draw_pixmap(widget->window,
+                    widget->style->fg_gc[GTK_WIDGET_STATE (widget)],
+                    panel->pixmap,
+                    ev->area.x, ev->area.y,
+                    ev->area.x, ev->area.y,
+                    ev->area.width, ev->area.height);
+    return 0;
+  }
 
 	GkrellmStyle			*style;
-	GkrellmTextstyle		*ts, *ts_alt;
+	GkrellmTextstyle		*ts;
 
 	if (first_create)
 		panel = gkrellm_panel_new0();
@@ -117,47 +105,29 @@ create_plugin(GtkWidget *vbox, gint first_create) {
 	style = gkrellm_meter_style(style_id);
 
 	ts = gkrellm_meter_textstyle(style_id);
-	ts_alt = gkrellm_meter_alt_textstyle(style_id);
 
-	decal_text1 = gkrellm_create_decal_text(panel, "8MHz", ts, style,
+	text = gkrellm_create_decal_text(panel, "8MHz", ts, style,
 					-1, -1, -1);
 
-	gkrellm_panel_configure(panel, NULL, style);
+	gkrellm_panel_configure(panel, 0, style);
 	gkrellm_panel_create(vbox, monitor, panel);
 
 	if (first_create)
 	    g_signal_connect(G_OBJECT (panel->drawing_area), "expose_event",
-    	        G_CALLBACK (panel_expose_event), NULL);
+    	        G_CALLBACK (panel_expose_event), 0);
 }
 
 
 static GkrellmMonitor	plugin_mon = {
-
-	CONFIG_NAME,		/* Name, for config tab.    */
-	0,			/* Id,  0 if a plugin       */
-	create_plugin,		/* The create function      */
-	update_plugin,		/* The update function      */
-	NULL,			/* The config tab create function   */
-	NULL,			/* Apply the config function        */
-
-	NULL,			/* Save user config			*/
-	NULL,			/* Load user config			*/
-	NULL,			/* config keyword			*/
-
-	NULL,			/* Undefined 2	*/
-	NULL,			/* Undefined 1	*/
-	NULL,			/* private		*/
-
-	MON_INSERT_AFTER|MON_CPU,	/* Insert plugin before this monitor			*/
-
-	NULL,			/* Handle if a plugin, filled in by GKrellM     */
-	NULL			/* path if a plugin, filled in by GKrellM       */
+  CONFIG_NAME, 0,
+  create_plugin, update_plugin,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  MON_INSERT_AFTER|MON_CPU
 };
 
 
 GkrellmMonitor *
 gkrellm_init_plugin() {
-
 	style_id = gkrellm_add_meter_style(&plugin_mon, STYLE_NAME);
 	monitor = &plugin_mon;
 	return &plugin_mon;
